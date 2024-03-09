0553b0bd2796ddd83e484a963bd93d0d7a08f3d2
Arif sandhi <93570402+Arifsandhi@users.noreply.github.com>
2024-03-09 17:50:22 +0000

    Study about Heap

---
+++ 01_basics/heap.js
@@ -0,0 +1,20 @@
+//Heap
+
+let user = {
+    email:"arif02@gmai.com",
+    user_name:"Arif"
+}
+
+let usertwo=user;
+
+usertwo.email="sandhi21@gmail.com";
+
+
+// in heap we sotre values and give output using refrense and HEAP(Non-premitive)
+
+console.log(user.email);
+console.log(usertwo.email);
+
+//if i change any value of any of the objects then that value change for every objects ..
+// but in stack system makes a copy and do not change real value.
+
